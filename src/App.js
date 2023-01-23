a81ef4d0160fe6602c5d4a3ad8b4758758baeb31
Anas <anasmahmud523@gmail.com>
2023-01-23 15:44:21 +0600

    data fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import User from './components/User';
 
 function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
   return (
     <div className="App">
+      {
+        users.map(user => <User
+          key={user.id}
+          user={user}
+        ></User>)
+      }
 
     </div>
   );
